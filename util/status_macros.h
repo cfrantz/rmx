7da435ae3df868bb73358ef4f8235553780f8a08
Chris Frantz <frantzcj@gmail.com>
2019-02-03 18:33:33 -0800

    Get status macros into the project.

---
+++ util/status_macros.h
@@ -0,0 +1,11 @@
+#ifndef UTIL_STATUS_MACROS_H
+#define UTIL_STATUS_MACROS_H
+
+// Get ASSIGN_OR_RETURN and RETURN_IF_ERROR
+//
+// ASSIGN_OR_RETURN(lhs, StatusOr<typeof lhs> returning expr)
+//
+// RETURN_IF_ERROR(Status-returning-expr)
+#include "google/protobuf/stubs/status_macros.h"
+
+#endif // UTIL_STATUS_MACROS_H
